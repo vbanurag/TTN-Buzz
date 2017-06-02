3161946e57e45e2477dcbd725195bd4a6efb42be
vbanurag <asharma.soccer@gmail.com>
2017-06-02 13:08:35 +0530

    config changes

--- Server/Config/cloud.upload.constants.js
+++ Server/Config/cloud.upload.constants.js
@@ -8,4 +8,5 @@ module.exports.cloudinary = {
     api_key: '547833665496258',
     api_secret: '_2GoOkH0xD9fXvudPTM2A5gUNPM'
 };
-module.exports.mongoDbURL = 'mongodb://localhost:27017/TTNBuzz';
+//module.exports.mongoDbURL = 'mongodb://localhost:27017/TTNBuzz';
+module.exports.mongoDbURL = 'mongodb://anurag:root@ds161551.mlab.com:61551/ttnbuzz';
