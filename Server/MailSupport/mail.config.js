0b5fd3e5559e2d40a0529b9902dbec9abd446506
vbanurag <asharma.soccer@gmail.com>
2017-06-02 13:29:46 +0530

    some changes

---
+++ Server/MailSupport/mail.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by anurag on 25/5/17.
+ */
+module.exports.mailAuth = {
+    user: 'noida.ttn@gmail.com',
+    pass: 'Anurag@2101'
+};
+
+module.exports.ComplaintResolvedOption = {
+    subject: 'Your Complaint has been resolved'
+};
